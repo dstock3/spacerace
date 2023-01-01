7f87f5c7861f98221af46b27dd1a50debcd8976b
dstock3 <dstock3@gmail.com>
2023-01-01 13:28:56 -0600

    Resolved conflicts in app file.

--- app.py
+++ app.py
@@ -1,3 +1,34 @@
+from flask import Flask, render_template
+from missions import get_mission_data
+from trivia import trivia_questions
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/trivia')
+def trivia():
+    return render_template('trivia.html', questions=trivia_questions)
+
+@app.route('/timeline')
+def timeline():
+    missions = get_mission_data()
+    return render_template('timeline.html', missions=missions)
+
+@app.route('/missions')
+def missions():
+    missions = get_mission_data()
+    return render_template('mission_list.html', missions=missions)
+
+@app.route('/mission/<int:id>')
+def mission_detail(id):
+    mission = get_mission_data(id)
+    if mission:
+        return render_template('mission_detail.html', mission=mission)
+    else:
+        return "Mission not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
