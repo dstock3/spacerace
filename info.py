fc3202cae4a2c92135d267adca623535d5bddda0
dstock3 <dstock3@gmail.com>
2023-01-26 20:21:25 -0600

    Added more links to info page.

--- info.py
+++ info.py
@@ -4,13 +4,33 @@ def get_info_data():
             'href': 'https://www.nasa.gov/',
             'desc': 'NASA'
         },
-                {
+        {
             'href': 'https://www.space.com/',
             'desc': 'Space.com'
         },
         {
             'href': 'https://history.nasa.gov/alsj/a11/images11.html',
             'desc': 'Apollo 11 Image Library'
+        },
+        {
+            'href': 'http://www.russianspaceweb.com/',
+            'desc': 'The Soviet Space Web'
+        },
+        {
+            'href': 'https://www.hq.nasa.gov/alsj/',
+            'desc': 'The Apollo Lunar Surface Journal'
+        },
+        {
+            'href': 'https://en.wikipedia.org/wiki/Space_Race',
+            'desc': 'The Space Race on Wikipedia'
+        },
+        {
+            'href': 'https://airandspace.si.edu/',
+            'desc': 'The National Air and Space Museum'
+        },
+        {
+            'href': 'https://www.flickr.com/photos/projectapolloarchive/albums/',
+            'desc': 'NASA\'s Apollo Archive'
         }
     ]
 
